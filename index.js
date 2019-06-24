54f0949eb999f8b6b25608cfe57440cd0ea2b90f
Boya <814686116@qq.com>
2019-06-24 17:29:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /** warning!!! 由于ES5的限制，ES6新增的Proxy无法被转译成ES5，或者通过Polyfill提供兼容。!!!warning
- * @author         boyaz.zhang@qunar.com
  * @name           dataProxy
  * @description    多层级数据结构取值容错处理&初始化默认值
  * @params         obj(any type)
